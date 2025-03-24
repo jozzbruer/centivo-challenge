d402feb83efcafd576c5d908b9a4b276e7f75b5a
Joz-Bruer Quince <quincejozbruer@gmail.com>
2025-03-23 17:17:16 -0700

    setup the project and Install dependencies

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+	try {
+		const connection = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`Database connected: ${connection.connection.host}`);
+	} catch (error) {
+		console.log('connection failed');
+	}
+};
+
+module.exports = connectDB;
